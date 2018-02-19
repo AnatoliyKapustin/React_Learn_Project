ed82412d0e672e913f7c24012a76bd491137021b
Anatoliy Kapustin <a.kapustin@infostroy.com.ua>
2018-02-19 18:22:23 +0200

    add timeline(not completed)

--- src/containers/Projects/ProjectsRoutes.js
+++ src/containers/Projects/ProjectsRoutes.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import {connect} from "react-redux";
 import {Menu} from "../../constants/Constants";
 import ListView from "./views/ListView";
 import TableView from "./views/TableView";
+import TimeLineView from "./views/TimeLineView";
+import {byId, filterIssuesByName, issuesInProject} from "../../helpers/dateFilter";
 
 class ProjectsRoutes extends Component {
 
@@ -17,22 +19,43 @@ class ProjectsRoutes extends Component {
         return (
             <Switch>
                 <Route exact path={"/projects"} component={() => (
-                    <ListView basePath={"/projects"}
+                    <ListView basePath="/projects"
                               issues={issues}
                               projects={projects}
                               selectedKey={Menu.LIST}
                               fullContent/>
                 )}/>
                 <Route exact path={"/projects/table"} component={() => (
-                    <TableView basePath={"/projects"}
+                    <TableView basePath="/projects"
                                issues={issues}
                                projects={projects}
                                selectedKey={Menu.TABLE}
                                fullContent/>
                 )}/>
-                <Route exact path="/projects/timeline"/>
+                <Route exact path={"/projects/timeline"} component={() => (
+                    <TimeLineView basePath="/projects"
+                                  issues={issues}
+                                  projects={projects}
+                                  selectedKet={Menu.TIMELINE}
+                                  fullContent/>
+                )}/>
                 <Route exact path="/projects/issues/:id"/>
-                <Route exact path="/projects/:id"/>
+                <Route exact path="/projects/:id" render={props => {
+                    let selectedId = props.match.params.id;
+                    let project = byId(projects, selectedId);
+                    if (!project) {
+                        return <Redirect to="/projects"/>
+                    }
+                    issues = issuesInProject(issues, project.id);
+                    return (
+                        <ListView headerText={project.name}
+                                  basePath={`/projects/${selectedId}`}
+                                  selectedProjectMenuItem={Menu.LIST}
+                                  issues={filterIssuesByName(issues, filterIssueName)}
+                                  users={users}
+                                  project={project}/>
+                    )
+                }}/>
 
                 <Route exact path="/projects/:id/table"/>
                 <Route exact path="/projects/:id/timeline"/>
---
+++ src/containers/Projects/views/TimeLineView.js
@@ -0,0 +1,41 @@
+import React, {Component} from "react";
+import {Col} from "react-bootstrap";
+
+import ProjectsView from "../../../components/projects/ProjectsView";
+import ProjectsFilters from "../../../components/projects/ProjectsFilters";
+import tableStyle from "../css/tableView.css";
+import style from "../css/style.css";
+
+class TimeLineView extends Component {
+
+    render() {
+
+        let {
+            basePath,
+            selectedKey,
+            headerText,
+            issues,
+            fullContent
+        } = this.props;
+
+        return (
+            <Col sm={10} className={`${style.mainContainer} ${style.fullHeight}`}>
+                <Col sm={fullContent ? 12 : 7} className={` ${style.projectsContainer} ${style.fullHeight}`}>
+                    <div className={` ${style.background} ${style.fullHeight}`}>
+                        <div className={style.mainHeader}>
+                            {headerText ? headerText : "Проекты"}
+                        </div>
+                        <ProjectsView elementStyle={tableStyle.tableView}
+                                      basePath={basePath}
+                                      selectedMenuItem={selectedKey}/>
+                        <ProjectsFilters textBeforeFilter={"ПОКАЗАТЬ"}/>
+
+                    </div>
+                </Col>
+            </Col>
+        )
+    }
+
+}
+
+export default TimeLineView;
