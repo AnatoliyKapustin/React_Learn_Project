c14cfc7762de56191a3c2f49e63561f2f76cf834
Anatoliy Kapustin <a.kapustin@infostroy.com.ua>
2018-02-28 19:32:10 +0200

    add components to routes

--- src/containers/Projects/ProjectsRoutes.js
+++ src/containers/Projects/ProjectsRoutes.js
@@ -28,8 +28,7 @@ class ProjectsRoutes extends Component {
                     <ListView basePath="/projects"
                               issues={issues}
                               projects={projects}
-                              selectedKey={Menu.LIST}
-                              fullContent/>
+                              selectedKey={Menu.LIST}/>
                 )}/>
                 <Route exact path={"/projects/table"} component={() => (
                     <TableView basePath="/projects"
@@ -51,15 +50,14 @@ class ProjectsRoutes extends Component {
                     if (!issue) {
                         return <Redirect to="/projects"/>
                     }
-
                     let project = byId(projects, selectedId);
-
                     return (
                         <ListView headerText={project ? headerText = project.name : null}
                                   basePath={"/projects"}
                                   issues={filterIssuesByName(issues, filterIssueName)}
                                   selectedIssueId={selectedId}
-                                  selectedKey={Menu.LIST}/>
+                                  selectedKey={Menu.LIST}
+                                  selectedIssue={issue}/>
                     )
                 }}/>
                 <Route exact path="/projects/:id" render={props => {
@@ -74,13 +72,37 @@ class ProjectsRoutes extends Component {
                                   basePath={`/projects/${selectedId}`}
                                   selectedKey={Menu.LIST}
                                   issues={filterIssuesByName(issues, filterIssueName)}
-                                  users={users}
-                                  project={project}/>
+                                  project={project}
+                                  selectedProject={project}/>
                     )
                 }}/>
 
-                <Route exact path="/projects/:id/table"/>
-                <Route exact path="/projects/:id/timeline"/>
+                <Route exact path="/projects/:id/table" render={props => {
+                    let selectedId = props.match.params.id;
+                    let project = byId(projects, selectedId);
+                    if (!project) {
+                        return <Redirect to="/projects"/>
+                    }
+                    issues = issuesInProject(issues, project.id);
+                    return (<TableView basePath={`/projects/${selectedId}`}
+                                       issues={issues}
+                                       projects={projects}
+                                       selectedKey={Menu.TABLE}/>
+                    )
+                }}/>
+                <Route exact path="/projects/:id/timeline" render={props => {
+                    let selectedId = props.match.params.id;
+                    let project = byId(projects, selectedId);
+                    if (!project) {
+                        return <Redirect to="/projects"/>
+                    }
+                    issues = issuesInProject(issues, project.id);
+                    return (<TimeLineView basePath={`/projects/${selectedId}`}
+                                          issues={issues}
+                                          projects={projects}
+                                          selectedKey={Menu.TIMELINE}/>
+                    )
+                }}/>
             </Switch>
         )
     }
