86338bea5bf2b082841b67d9065ccad5036ca196
Anatoliy Kapustin <a.kapustin@infostroy.com.ua>
2018-02-28 19:30:43 +0200

    fix double image

--- src/components/issues/DoubleImageIssueItem.js
+++ src/components/issues/DoubleImageIssueItem.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 
+import undefinedAvatar from "../../../public/images/defaultAvatar.png"
+
 import style from "../issues/css/style.css"
+import {Image} from "react-bootstrap";
 
 class IssueDoubleImage extends Component {
 
@@ -11,11 +14,15 @@ class IssueDoubleImage extends Component {
             executorAvatar
         } = this.props;
 
-        this.return(<div>
+        return (
+            <div className={style.doubleImagesContainer}>
                 <Image src={authorAvatar} className={`${style.issueDoubleImageBottom} rounded-0`} alt="avatar"
                        responsive/>
-                <Image src={executorAvatar} className={`${style.issueDoubleImageTop} rounded-0`} alt="avatar"
+                <Image src={executorAvatar ? executorAvatar : undefinedAvatar}
+                       className={`${style.issueDoubleImageTop} ${executorAvatar ? "" : style.undefinedAvatar} rounded-0`}
+                       alt="avatar"
                        responsive/>
+
             </div>
         )
     };
