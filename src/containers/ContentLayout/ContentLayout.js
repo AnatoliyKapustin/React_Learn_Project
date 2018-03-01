506de90913ad341a3f090f77cd7eba3f9e2a9637
Anatoliy Kapustin <a.kapustin@infostroy.com.ua>
2018-03-01 13:30:28 +0200

    add issues routes

--- src/containers/ContentLayout/ContentLayout.js
+++ src/containers/ContentLayout/ContentLayout.js
@@ -1,16 +1,14 @@
 import React, {Component} from "react";
 import {Route, Switch} from "react-router-dom";
-import Issues from "../../containers/Issues/Issues"
 import ProjectsRoutes from "../Projects/ProjectsRoutes";
+import IssuesRoutes from "../Issues/IssuesRoutes";
 
 class ContentLayout extends Component {
 
     render() {
         return (
             <Switch>
-                <Route path="/issues" component={() => (
-                    <Issues fullContent/>
-                )}/>
+                <Route path="/issues" component={IssuesRoutes}/>
                 <Route path="/projects" component={ProjectsRoutes}/>
             </Switch>
         )
